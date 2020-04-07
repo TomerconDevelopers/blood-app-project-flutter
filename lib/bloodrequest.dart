d2ca532ab484d97d649bbbe1d7035536b4454031
duaboola <50785323+duaboola@users.noreply.github.com>
2020-04-07 19:00:46 +0530

    Add files via upload

---
+++ lib/bloodrequest.dart
@@ -0,0 +1,13 @@
+class Request {
+  String name;
+  String bloodgroup = '';
+  int age;
+  DateTime date;
+  int number;
+  String phone = '';
+  String phone_extra = '';
+  String district = '';
+  String taluk;
+  String hospital;
+
+}
