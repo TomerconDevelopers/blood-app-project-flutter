cf29470eea68f11c0deb6a4ff6f8d68df94d57a4
TomerconDevelopers <tomercondevelopers@gmail.com>
2020-04-13 19:18:31 +0530

    Fixed global variable issue.checked signup and login

--- lib/globals.dart
+++ lib/globals.dart
@@ -7,6 +7,7 @@ String app_version = "0.01-a";
 String baseurl = "http://vps001.qubehost.com/bloodapp/"; //Need to update after setup server
 Color prim_color = Colors.red[400];//Color for appbar
 String bg_image = "assets/images/bg.png";//background image for all pages
+String cu_id;
 
 class UserLocationTracker {
 
