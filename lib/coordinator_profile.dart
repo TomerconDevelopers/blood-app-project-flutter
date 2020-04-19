32f882c2f7c3688d2f67c42c2b0bdb0073425af1
sandrasmenon18 <40963950+sandrasmenon18@users.noreply.github.com>
2020-04-19 19:23:34 +0530

    Update coordinator_profile.dart

--- lib/coordinator_profile.dart
+++ lib/coordinator_profile.dart
@@ -2,17 +2,19 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'globals.dart' as g;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'utils.dart' as ut;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class CoordinatorProfile extends StatefulWidget {
+  CoordinatorProfile({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _CoordinatorProfileState createState() => _CoordinatorProfileState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CoordinatorProfileState extends State<CoordinatorProfile> {
   Map f={};
   String u=g.baseUrl;
   @override
@@ -20,7 +22,7 @@ class _MyHomePageState extends State<MyHomePage> {
     postValues();
     super.initState();
   }
-  String uname = '';
+  String uname = '',pass='';
   var r;
   String n = '',
       pr='',
@@ -33,7 +35,10 @@ class _MyHomePageState extends State<MyHomePage> {
       un = '';
       //function to post userid and password and accepts full details
 postValues() async {
-    var bd = json.encode({"userid": uname}); 
+  final SharedPreferences spp=await SharedPreferences.getInstance();
+    uname=spp.getString('username');
+    pass=spp.getString('password');
+    var bd = json.encode({"userid": uname,"pass":pass}); 
     var response = await http.post(
         u+"/coordinator_profile.php",
         body: bd);
@@ -55,113 +60,114 @@ postValues() async {
   }
   @override
   Widget build(BuildContext context) {
-    f = ModalRoute.of(context).settings.arguments;
-    uname=f['username'];
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed:(){},child: Icon(Icons.edit),backgroundColor:Colors.deepOrange ,),
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-      body: ListView(
-        children: <Widget>[
-        //background settings for profile pic
-          Container(
-            height: 200,
-              decoration: BoxDecoration(
-              image: DecorationImage(
-              image: AssetImage("images/blood_doodle.jpg"),
-              fit: BoxFit.cover,
-             colorFilter: new ColorFilter.mode(
-                  Colors.black.withOpacity(0.2), BlendMode.dstATop),),
-                color: Colors.deepOrange.shade300,
-          ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+    return MaterialApp(
+      theme: ut.maintheme(),
+          home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed:(){},child: Icon(Icons.edit),backgroundColor:Colors.deepOrange ,),
+        appBar: AppBar(
+          title: Text('Coordinator Profile'),
+        ),
+        body: ListView(
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-              //profile picture
-                CircleAvatar(
-                  minRadius: 60,
-                  backgroundColor: Colors.deepOrange.shade300,
-                  child: CircleAvatar(
-                    //backgroundImage: new AssetImage("images/profile.jpg"),
-                    minRadius: 50,
-                  ),
-                ),
-              ],
+          //background settings for profile pic
+            Container(
+              height: 200,
+                decoration: BoxDecoration(
+                image: DecorationImage(
+                image: AssetImage("assets/images/blood_doodle.jpg"),
+                fit: BoxFit.cover,
+               colorFilter: new ColorFilter.mode(
+                    Colors.black.withOpacity(0.2), BlendMode.dstATop),),
+                  color: Colors.deepOrange.shade300,
             ),
-            //Name and profession details
-            SizedBox(height: 10,),
-            Text(n.toUpperCase(), style: TextStyle(fontSize: 22.0, color: Colors.white,fontWeight: FontWeight.bold),),
-            Text(pr, style: TextStyle(fontSize: 14.0, color: Colors.white),)
-          ], ),
-          ),
-          
-          Container(
-            // height: 50,
-            child: Row(
-              children: <Widget>[
-              //Experience details
-                Expanded(
-                  child: Container(
-                    color: Colors.deepOrange.shade400,
-                    child: ListTile(
-                      title: Text("$ex years",textAlign: TextAlign.center, style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24.0
-                      ),),
-                      subtitle: Text("of trusted service", textAlign: TextAlign.center, style: TextStyle(color: Colors.white,
-                      fontStyle: FontStyle.italic),),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                //profile picture
+                  CircleAvatar(
+                    minRadius: 60,
+                    backgroundColor: Colors.deepOrange.shade300,
+                    child: CircleAvatar(
+                      backgroundImage: new AssetImage("assets/images/profile.jpg"),
+                      minRadius: 50,
                     ),
                   ),
-                ),
-                //Details of donors' verified
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    child: ListTile(
-                      title: Text(re,textAlign: TextAlign.center, style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24.0
-                      ),),
-                      subtitle: Text("verified donor requests", textAlign: TextAlign.center, style: TextStyle(color: Colors.white,
-                          fontStyle: FontStyle.italic),),
+                ],
+              ),
+              //Name and profession details
+              SizedBox(height: 10,),
+              Text(n.toUpperCase(), style: TextStyle(fontSize: 22.0, color: Colors.white,fontWeight: FontWeight.bold),),
+              Text(pr, style: TextStyle(fontSize: 14.0, color: Colors.white),)
+            ], ),
+            ),
+            
+            Container(
+              // height: 50,
+              child: Row(
+                children: <Widget>[
+                //Experience details
+                  Expanded(
+                    child: Container(
+                      color: Colors.deepOrange.shade400,
+                      child: ListTile(
+                        title: Text("$ex years",textAlign: TextAlign.center, style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24.0
+                        ),),
+                        subtitle: Text("of trusted service", textAlign: TextAlign.center, style: TextStyle(color: Colors.white,
+                        fontStyle: FontStyle.italic),),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  //Details of donors' verified
+                  Expanded(
+                    child: Container(
+                      color: Colors.red,
+                      child: ListTile(
+                        title: Text(re,textAlign: TextAlign.center, style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24.0
+                        ),),
+                        subtitle: Text("verified donor requests", textAlign: TextAlign.center, style: TextStyle(color: Colors.white,
+                            fontStyle: FontStyle.italic),),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          
-          //Other details
-          ListTile(
-            title: Text("Coordinator ID", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
-            subtitle: Text(un, style: TextStyle(fontSize: 18.0),),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("Phone", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
-            subtitle: Text(c, style: TextStyle(fontSize: 18.0),),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("Email", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
-            subtitle: Text(e, style: TextStyle(fontSize: 18.0),),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("Taluk", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
-            subtitle: Text(l, style: TextStyle(fontSize: 18.0),),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("District", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
-            subtitle: Text(d, style: TextStyle(fontSize: 18.0),),
-          ),
-          Divider(),
-        ],
-      )
+            
+            //Other details
+            ListTile(
+              title: Text("Coordinator ID", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
+              subtitle: Text(un, style: TextStyle(fontSize: 18.0),),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Phone", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
+              subtitle: Text(c, style: TextStyle(fontSize: 18.0),),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Email", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
+              subtitle: Text(e, style: TextStyle(fontSize: 18.0),),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Taluk", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
+              subtitle: Text(l, style: TextStyle(fontSize: 18.0),),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("District", style: TextStyle(color: Colors.deepOrange, fontSize: 12.0),),
+              subtitle: Text(d, style: TextStyle(fontSize: 18.0),),
+            ),
+            Divider(),
+          ],
+        )
+      ),
     );
   }
 }
